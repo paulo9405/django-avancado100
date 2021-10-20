9b85152560fa2cb11ed736b3719f640c99948d65
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-10-20 20:01:34 -0300

    config login em redes sociais

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -33,8 +33,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 
     'allauth.socialaccount.providers.facebook',
-    # 'allauth.socialaccount.providers.twitter',
-    # 'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.google',
 
     'clientes',
     'home',
