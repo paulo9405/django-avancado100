ec3d170b7aaebcb4b3feac61ca0306dd1204aa5a
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-10-29 18:49:04 -0300

    removendo um item do pedido

--- vendas/urls.py
+++ vendas/urls.py
@@ -5,7 +5,8 @@ from .views import (
     NovoItemPedido,
     ListaVendas,
     EditPedido,
-    DeletePedido
+    DeletePedido,
+    DeleteItemPedido,
     )
 
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path('novo-pedido/', NovoPedido.as_view(), name="novo-pedido"),
     path('novo-item-pedido/<int:venda>/', NovoItemPedido.as_view(), name="novo-item-pedido"),
     path('edit-pedido/<int:venda>/', EditPedido.as_view(), name="edit-item-pedido"),
+    path('delete-item-pedido/<int:item>/', DeleteItemPedido.as_view(), name="delete-item-pedido"),
     path('delete-pedido/<int:venda>/', DeletePedido.as_view(), name="delete-pedido"),
     path('dashboard/', DashboardView.as_view(), name="dashboard"),
 ]
